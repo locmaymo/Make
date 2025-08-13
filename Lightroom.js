788ddc72944b5e5e58dc4e2a5e58f64edfd1d02d
Phạm Quang Lộc <locmaymo@gmail.com>
2025-08-13 21:51:45 +0700

    Update purchase date for Lightroom subscription

--- Lightroom.js
+++ Lightroom.js
@@ -4,7 +4,7 @@ obj.entitlement.status="subscriber";
 obj.current_subs={
       "product_id": "lightroom",
       "store": "adobe",
-      "purchase_date": "2019-10-10T16:32:10.254954Z",
+      "purchase_date": "2025-08-13T14:51:03.185Z",
       "sao": {
         "inpkg_CCES": "0",
         "inpkg_CCLE": "1",
